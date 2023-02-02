26a254732b2b3871713cf22d2a7eceae197fd252
Robert <rlgallegos85@gmail.com>
2023-02-02 14:55:21 -0500

    dom content loaded

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+// // Your code goes here
+
+// Code your solution in index.js. First, set up a DOMContentLoaded event listener to detect when the HTML
+//  page has loaded and the document is ready to be manipulated. Use the event's 
+//  callback function to target the paragraph element with id="text" and replace the text with 
+//  "This is really cool!"
+
+// Note: Using the innerText Links to an external site.property to modify DOM element content will 
+// not work for this lab. Use textContent Links to an external site.or innerHTML Links to an external site.instead.
+
+document.addEventListener('DOMContentLoaded', () => {
+    let id = document.getElementById('text');
+    console.log("DoM Content succesfully Loaded")
+    id.textContent = "This is really cool!";
+})
